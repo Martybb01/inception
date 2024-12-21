aaf8f61fe71d6b4edc84e956f4bb110d80fdd6c4
Martina Boccuzzi <martybb01@gmail.com>
2024-12-21 20:09:36 +0100

    setup mariadb

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DB_PASS_ROOT=superpippo
+DB_USER=marboccu
+DB_PASSWORD=pippo
+DB_NAME=mariadb_db
+
+mysqld_safe --datadir="/var/lib/mysql" &
+sleep 5
+
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%'       IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT'
+    WITH GRANT OPTION;
+
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+FLUSH PRIVILEGES;
+EOF
+
+mysqladmin -u root -p"$DB_PASS_ROOT" shutdown
+exec mysqld_safe --datadir="/var/lib/mysql"
