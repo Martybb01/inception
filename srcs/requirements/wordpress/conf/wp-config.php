b97d78d42bf6ef8111cfe839c242e217e6085852
Martina Boccuzzi <martybb01@gmail.com>
2024-12-21 20:37:02 +0100

    setup wordpress container

---
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -0,0 +1,6 @@
+define( 'DB_NAME', getenv('mariadb_db') );
+define( 'DB_USER', getenv('marboccu') );
+define( 'DB_PASSWORD', getenv('pippo') );
+define( 'DB_HOST', getenv('mariadb') );
+define( 'WP_HOME', getenv('https://marboccu.42.fr') );
+define( 'WP_SITEURL', getenv('https://marboccu.42.fr') );
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WP_URL=marboccu.42.fr
+WP_TITLE=Inception
+WP_ADMIN_USER=theroot
+WP_ADMIN_PASSWORD=root
+WP_ADMIN_EMAIL=theroot@yolo.com
+WP_USER=marboccu
+WP_PASSWORD=pippo
+WP_EMAIL=marboccu@yolo.com
+WP_ROLE=editor
+
+chown -R www-data:www-data /var/www/inception/
+
+if [ ! -f /var/www/inception/wp-config.php ]; then
+	mv /tmp/wp-config.php /var/www/inception/
+fi
+
+sleep 10
+wp --allow-root --path="/var/www/inception/" core download || true
+
+if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+then
+    wp  --allow-root --path="/var/www/inception/" core install \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
+fi;
+
+if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
+then
+    wp  --allow-root --path="/var/www/inception/" user create \
+        $WP_USER \
+        $WP_EMAIL \
+        --user_pass=$WP_PASSWORD \
+        --role=$WP_ROLE
+fi;
+
+exec $@
